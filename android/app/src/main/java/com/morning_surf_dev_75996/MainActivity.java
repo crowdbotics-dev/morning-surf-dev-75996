63ee824e314e2098203af0555af886c35220cd92
Crowdbotics <engineering@crowdbotics.com>
2023-04-12 00:08:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/morning_surf_dev_75996/MainActivity.java
@@ -0,0 +1,15 @@
+package com.morning_surf_dev_75996;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "morning_surf_dev_75996";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-surf-dev-75996",
+  url: "https://morning-surf-dev-75996.botics.co",
+  api: "https://morning-surf-dev-75996.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
